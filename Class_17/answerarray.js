a22df4c32389652ff8b2368693455eff8b525257
Daan <daan@bogaerts.com>
2018-04-25 10:32:30 +0200

    Changes by filippo and daan

---
+++ Class_17/answerarray.js
@@ -0,0 +1,5 @@
+var food = ['pizza', 'burger', 'pasta', 'fries', 'risotto', 'frikandel', 'boerenkool', 'iskender', 'bitter ballen', 'gozleme']
+
+for(i=0; i<10; i++) {
+  console.log(food[0])
+}
---
+++ Class_17/answers.js
@@ -0,0 +1,15 @@
+var food = ['pizza', 'burger', 'pasta', 'fries', 'risotto', 'frikandel', 'boerenkool', 'iskender', 'bitter ballen', 'gozleme']
+
+for(i=0; i<10; i++) {
+  console.log(food[i])
+}
+
+
+var food = ['pizza', 'burger', 'pasta', 'fries', 'risotto', 'frikandel', 'boerenkool', 'iskender', 'bitter ballen', 'gozleme']
+
+var l = 0;
+  var foodLenght = food.length;
+  while (l<foodLenght){
+    console.log(food[l])
+    l++
+  }
