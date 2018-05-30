7bf2948474d290223fe68d8ac7bd37186d1a6dad
swellingtonswellaway <alice@ALICEs-MBP.localdomain>
2018-05-30 10:19:01 +0200

    cleanup day2

---
+++ day2/javascript-basics-day2.js
@@ -0,0 +1,17 @@
+//loops
+
+let counter = 5;
+while (counter < 50){
+  counter +=5;
+  console.log(counter);
+  //well done! this is correct
+
+}
+
+//arrays
+
+let fruits = ['apples', 'oranges', 'pears', 'apricots'];
+let i = 0;
+
+
+console.log(`I love: ${fruits}`);
