fa2890ce1f9e1c9a8028b00f1351afcd160df4e3
Max van Deurzen <38442626+mivd7@users.noreply.github.com>
2018-06-02 13:38:40 +0200

    git sucks balls
    but I finished section 1

---
+++ class_18/your_javascript_here.js
@@ -0,0 +1,25 @@
+// Variables
+let hero = {
+  name: 'Supermax',
+  heroic: true,
+  inventory: ['a','b'],
+  health: 100
+}
+
+hero.weapon = {
+  type: 'AK-47',
+  damage: 100
+}
+
+console.log(hero)
+// Game logic
+//
+//Declare the global variable hero and assign it an object. This object has the following keys and values:
+
+//1. `name` which is a string (you choose the name)
+//2. `heroic` which is a boolean
+//3. `inventory` which is an array
+//4. `health` which is a number
+//5. `weapon` which is an object (it has some stats in the form keys and values)
+//6. `weapon` has a key `type` which is a string
+//7. `weapon` has a key `damage` which is a number
